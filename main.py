31c30cdaabfcbc804b36373d06dcccd0a094590c
Krystian Pietryka <krystian.pietryka@ggsitc.com>
2024-05-25 21:10:09 +0200

    added faction tiles and home worlds with mechanisms to distribute them

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import json 
+import copy
+import random
 from player_state_class import Player_State
 from game_state_class import Game_State
 from planet_class import Planet
 from development_class import Development
 
+#add faction developments
+#implement development actions
+
+
 # planets_file = open('planets.json')
 # planets_dict = json.load(planets_file)
 
-def load_developments(game_state):
+def load_bag_developments(game_state):
     developments = []
     developments_file = open('developments.json')
     developments_dict = json.load(developments_file)
@@ -16,7 +22,7 @@ def load_developments(game_state):
         developments.append(d)
     game_state.development_bag = developments
 
-def load_planets(game_state):
+def load_bag_planets(game_state):
     planets = []
     planets_file = open('planets.json')
     planets_dict = json.load(planets_file)
@@ -24,6 +30,50 @@ def load_planets(game_state):
         p = Planet(planet['name'], planet['cost'], planet['credit_gain'], planet['remove_dice'], planet['gained_dice_color'], planet['gained_dice_type'] )
         planets.append(p)
     game_state.planet_bag = planets
+
+def distribute_faction_tiles_to_players(game_state):
+    faction_tiles = []
+    faction_tiles_file = open('faction_tiles.json')
+    faction_tiles_dict = json.load(faction_tiles_file)
+    for tile in faction_tiles_dict:
+        faction_tiles.append(tile)
+    for player in game_state.players:
+        current_player_planet_stack = copy.deepcopy(player.planet_stack)
+        current_player_development_stack = copy.deepcopy(player.development_stack)
+        random_faction_tile = random.choice(faction_tiles)
+        random_faction_tile_planets = random_faction_tile.get("planet")
+        # print(random_faction_tile_planets)
+        # print(isinstance(random_faction_tile_planets, list))
+        random_faction_tile_developments = random_faction_tile.get("development")
+        if random_faction_tile_planets:
+                if isinstance(random_faction_tile_planets, list):
+                     for p in random_faction_tile_planets:
+                          current_player_planet_stack.append(p)
+                else:
+                    current_player_planet_stack.append(random_faction_tile_planets)
+        if random_faction_tile_developments:
+                if isinstance(random_faction_tile_developments, list):
+                     for d in random_faction_tile_developments:
+                          current_player_development_stack.append(d)
+                else:
+                    current_player_development_stack.append(random_faction_tile_developments)
+        player.planet_stack = current_player_planet_stack
+        player.development_stack = current_player_development_stack
+        faction_tiles.remove(random_faction_tile)
+
+def distribute_home_world_planets_to_players(game_state):
+    planets = []
+    planets_file = open('home_world_planets.json')
+    planets_dict = json.load(planets_file)
+    for planet in planets_dict:
+        p = Planet(planet['name'], planet['cost'], planet['credit_gain'], planet['remove_dice'], planet['gained_dice_color'], planet['gained_dice_type'] )
+        planets.append(p)
+    for player in game_state.players:
+        current_player_planet_stack = copy.deepcopy(player.planet_stack)
+        random_home_world_planet = random.choice(planets)
+        current_player_planet_stack.append(random_home_world_planet.name)
+        player.planet_stack = current_player_planet_stack
+        planets.remove(random_home_world_planet)
 
 def create_starting_players(game_state):
     players = []
@@ -34,11 +84,13 @@ def create_starting_players(game_state):
 def create_starting_game_state(amount_of_players):
     current_game_state = Game_State(amount_of_players)
     create_starting_players(current_game_state)
-    load_developments(current_game_state)
-    load_planets(current_game_state)
+    load_bag_developments(current_game_state)
+    load_bag_planets(current_game_state)
     print(current_game_state)
+    distribute_home_world_planets_to_players(current_game_state)
+    distribute_faction_tiles_to_players(current_game_state)
     current_game_state.print_players()
     #current_game_state.print_development_bag()
-    current_game_state.print_planet_bag()
+    #current_game_state.print_planet_bag()
 
 create_starting_game_state(3)
--- player_state_class.py
+++ player_state_class.py
@@ -1,5 +1,5 @@
 class Player_State:
-    def __init__(self, name, credits, victory_points, tile_amount, dice_in_population, dice_in_cup, all_dice, tiles_played, planets_stack, developments_stack):
+    def __init__(self, name, credits, victory_points, tile_amount, dice_in_population, dice_in_cup, all_dice, tiles_played, planet_stack, development_stack):
         self.name = name
         self.credits = credits
         self.victory_points = victory_points
@@ -8,7 +8,7 @@ class Player_State:
         self.dice_in_cup = dice_in_cup
         self.all_dice = all_dice
         self.tiles_played = tiles_played
-        self.planets_stack = planets_stack
-        self.developments_stack = developments_stack
+        self.planet_stack = planet_stack
+        self.development_stack = development_stack
     def __repr__(self):
-        return f"{self.name}: Credits: {self.credits}, Victory_Points: {self.victory_points}, Tiles Played: {self.tiles_played}, Dice in cup: {self.dice_in_cup}, Dice in Population: {self.dice_in_population}, Planet Stack: {self.planets_stack}, Development Stack: {self.developments_stack}"
+        return f"{self.name}: Credits: {self.credits}, Victory_Points: {self.victory_points}, Tiles Played: {self.tiles_played}, Dice in cup: {self.dice_in_cup}, Dice in Population: {self.dice_in_population}, Planet Stack: {self.planet_stack}, Development Stack: {self.development_stack}"
